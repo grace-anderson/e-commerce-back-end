8712e74c0b4f3e55a74eeac8761b4ad8231df7b6
Grace <grace.anderson.au@gmail.com>
2022-05-18 19:44:31 +1000

    fixed error with not found - added return

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,36 +1,44 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
-// const { Category } = require("../../models");
 
 // The `/api/categories` endpoint
 
 router.get("/", async (req, res) => {
+  // get all categories
+  // be sure to include associated Products
   try {
-    const categories = await Category.findAll(
-      {
-        include: [{ model: Product }],
-      }
-    );
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
     res.status(200).json(categories);
   } catch (error) {
     res.status(500).json(error);
   }
-  // be sure to include its associated Products
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
-  // try {
-  //   const category = await Category.findByPk(req.params.id);
-  //   res.status(200).json(category);
-  // } catch (error) {
-  //   res.status(500).json(error);
-  // }
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!category) {
+      res.status(404).send({message: "Not found"});
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
   // create a new category
+
+
 });
 
 router.put("/:id", (req, res) => {
